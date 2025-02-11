8759fe9cfe2a649415524493d24660053961275c
loopsandlogic <loopsandlogic@gmail.com>
2025-02-10 20:32:31 -0600

    Updated

--- Ch10_Oops/classes.py
+++ Ch10_Oops/classes.py
@@ -1,38 +1,40 @@
 class Student:
-    name = 'Dinesh'
+
     cls = 10
     sec = 'A'
-    age = 15
+
+    def __call__(self, *args, **kwds):
+        print('Under call method')
+
+    def __format__(self, format_spec):
+        if format_spec == "name":
+            return f"Person's Name: {self.name}"
+        elif format_spec == "age":
+            return f"Person's Age: {self.age}"
+        else:
+            return f"{self.name}, {self.age} years old"
+    
+    def __dir__(self):
+        return ['msg']
 
     def __init__(self, name, age):
         self.name = name
         self.age = age
         print('New Student created')
 
+    @staticmethod
+    def msg():
+        print('Hi, I am a student!')
+
+
 s1 = Student('Dinesh', 15)
-
-print(s1.name)
-print(s1.age)
-
 s2 = Student('Ranjeet', 16)
-print(s2.name)
-print(s2.age)
 
-# s1_name = 'Dinesh'
-# s1_age = 15
-# s1_cls = 10
-# s1_sec = 'A'
+s1()
 
-# s2_name = 'Ranjeet'
-# s2_age = '16'
-# s2_cls = '10'
-# s2_sec = 'A'
+print(format(s1, name))
+print(f"{s1:age}") 
 
-# def details(name, age):
-#         print('Student detail is name:', name, 'age:', age )
+print(dir(s1))
 
-
-# details(s1_name, s1_age)
-# details(s2_name, s2_age)
-
-# s1
+s1.msg()
